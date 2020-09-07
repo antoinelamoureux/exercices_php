e24fcafdd1066a229330340dbce118eabcf257a1
antoinelamoureux <67148089+antoinelamoureux@users.noreply.github.com>
2020-09-07 12:13:56 +0200

    Calculator update

--- calculator.php
+++ calculator.php
@@ -6,23 +6,52 @@
     <title>Document</title>
 </head>
 <body>
-<form action="POST">
+<form method="POST" action="calculator.php">
 <input type="text">
 <?php
 $numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+$signs = array('+', '-', '*', '-', '/', '%');
 for ($i = 0; $i < count($numbers); $i++) {
-    echo "<input type=\"button\" value =\"$numbers[$i]\">";
+    echo "<input name=\"$numbers[$i]\" type=\"button\" value =\"$numbers[$i]\">";
 }
+for ($i = 0; $i < count($signs); $i++) {
+    echo "<input name=\"$signs[$i]\" type=\"button\" value =\"$signs[$i]\">";
+}
+
 ?>
-<input type="submit" value="calculer">
+<input type="submit" name="submit" value="calculer">
 </form>
 <?php
 
+var_dump($_POST);
 
-
-function add() {
- 
+function add($number1, $number2) {
+    $resultat = $number1 + $number2; 
+    return $resultat;
 }
+
+function substact($number1, $number2) {
+    $resultat = $number1 - $number2;
+    return $resultat;
+}
+
+function multiply($number1, $number2) {
+    $resultat = $number1 * $number2;
+    return $resultat;
+}
+
+function divide($number1, $number2) {
+    $resultat = $number1 / $number2;
+    return $resultat;
+}
+
+function modulo($number1, $number2) {
+    $resultat = $number1 % $number2;
+    return $resultat;
+}
+
+$number1 = 5;
+$number2 = 2;
 
 ?>
     
